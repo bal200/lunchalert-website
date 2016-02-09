302b8b62f7b351d37e81c83d6ab457f6e9e5ad27
Bal <bal200@gmail.com>
2016-02-09 16:24:47 +0000

    start of the potal page.  So far, test code to link to parse, and 1st try at a google map.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@ angular.module('lunchalert-website', [
     'pageCtrl'
   ])
 
+.value('ParseConfiguration', {
+    /* DEV */
+    applicationId: "uKWkJW0IFFhthG7e3A1NPqh2JhazzKEfZD7d1RXr",
+    javascriptKey: "gXgY9Kf8wafGGU3WghwqrEWMfbrl7oi7e27om7J6"
+})
+    
 .config(['$routeProvider',
   function($routeProvider) {
     $routeProvider
@@ -30,7 +36,37 @@ angular.module('lunchalert-website', [
         templateUrl: 'templates/install.htm',
         controller: 'pageCtrl'
       })
+      .when('/login', {
+        templateUrl: 'templates/login.htm',
+        controller: 'userCtrl'
+      })
+      .when('/portal', {
+        templateUrl: 'templates/portal.htm',
+        controller: 'portalCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
-  }]);
+  }
+])
+
+.run(function ($rootScope, ParseConfiguration) {
+			
+      Parse.initialize(ParseConfiguration.applicationId, ParseConfiguration.javascriptKey);
+			var currentUser = Parse.User.current();
+			$rootScope.user = null;
+			$rootScope.isLoggedIn = false;
+
+			if (currentUser) {
+				$rootScope.user = currentUser;
+				$rootScope.isLoggedIn = true;
+				//$state.go('app.home');
+			}
+});
+
+var mapInited=false;
+function initMap(){
+  mapInited=true;
+}
+
+
--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,6 @@
 angular.module('pageCtrl',[])
-.controller('pageCtrl', ['$scope', function($scope) {
+.controller('pageCtrl', ['$scope',
+function($scope) {
  
   $scope.selectChange = function() {
     switch ($scope.vans) {
@@ -38,10 +39,112 @@ angular.module('pageCtrl',[])
     }else{
       $scope.summary="";
     }
-   
-   
     
   }
   
+}])
+
+
+.controller('userCtrl', ['$scope', '$rootScope', '$location',
+function( $scope, $location, $rootScope ) {
+  $scope.user = {
+    username: null,
+    password: null
+  };
+
+  $scope.error = {};
+
+  $scope.login = function() {
+    var user = $scope.user;
+    Parse.User.logIn(('' + user.username).toLowerCase(), user.password, {
+      success: function(user) {
+        $rootScope.user = user;
+        $rootScope.isLoggedIn = true;
+        /* Move to Home screen, but change the back button to the home button */
+
+        $location.path('/portal');
+        
+      },
+      error: function(user, err) {
+        // The login failed. Check error to see why.
+        if (err.code == 101) {
+          alert('Login failed');
+        } else if (err.code == 100) {
+          alert('No internet connection');
+        } else {
+          $scope.error.message = 'An unexpected error has ' +
+              'occurred. Code '+err.code + ' ' + err.message;
+          console.log('Login failed: ' + err.message);
+        }
+        $scope.$apply();
+      }
+    });
+    return false;
+  };
+
+  $scope.forgot = function() {
+    //$state.go('app.forgot');
+  };
+}])
+
+
+.controller('portalCtrl', ['$scope', '$rootScope', '$location',
+function( $scope, $location, $rootScope ) {
+var beaches = [
+  ['Bondi Beach', -33.890542, 151.274856, 4],
+  ['Coogee Beach', -33.923036, 151.259052, 5],
+  ['Cronulla Beach', -34.028249, 151.157507, 3],
+  ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+  ['Maroubra Beach', -33.950198, 151.259302, 1]
+];
+
+
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: {lat: -33.9, lng: 151.2}
+  });
+
+  for (var i = 0; i < beaches.length; i++) {
+    var beach = beaches[i];
+    var marker = new google.maps.Marker({
+      position: {lat: beach[1], lng: beach[2]},
+      map: map,
+      title: beach[0]
+      //zIndex: beach[3]
+    });
+  }
+
+
+
+  // Get the Arrivals data from the Parse server
+  var Arrival = Parse.Object.extend("Arrival");
+  var query = new Parse.Query(Arrival);
+  var usr = Parse.User.current();
+  query.equalTo('vendor', usr);
+  /* @Todo: Date range */ 
+  query.find({
+    success: function(res) { 
+      //alert("Successfully retrieved " + res.length + " scores.");
+      for (var i = 0; i < res.length; i++) {
+        var object = res[i];
+        //alert(object.id + ' - ' + object.get('location').latitude);
+      }
+      
+      
+    },
+    error: function(err) { alert("get arrivals error: "+err.code+" "+err.message); }
+  });
+
+
+
+
+
 }]);
+
+
+
+
+
+
 
