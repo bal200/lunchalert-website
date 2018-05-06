366e3cc9874409cdc8cf78e4dc4f2aabe797dfa0
Bal <bal200@gmail.com>
2018-05-06 20:57:24 +0100

    more work on Cards & Campaigns page

--- portal_admin/js/cards.controller.js
+++ portal_admin/js/cards.controller.js
@@ -5,6 +5,7 @@ angular
 CardsController.$inject = ['$scope', '$location', '$rootScope'];
 function CardsController($scope, $location, $rootScope) {
   
+  /*********************** CARD *****************************************/
   var Card = Parse.Object.extend("Card", {
   },{ create: function (vendor, title) {
     var card = new Card();
@@ -19,7 +20,22 @@ function CardsController($scope, $location, $rootScope) {
   Card.prototype.__defineSetter__("active", function(val) {return this.set("active",val)});
   Card.prototype.__defineGetter__("order", function() {return this.get("order");});
   Card.prototype.__defineSetter__("order", function(val) {return this.set("order",val)});
-  
+  Card.prototype.__defineGetter__("type", function() { return ( (this.get("html"))?"custom":"simple" ); });
+  Card.prototype.__defineSetter__("type", function(val) {console.log("@TODO: custom/simple cards")});
+
+  Card.prototype.__defineGetter__("schedulerOn", function() {
+    if (!this.campaign) { return false; }
+    else { return this.campaign.get("active"); }
+  });
+  Card.prototype.__defineSetter__("schedulerOn", function(val) {
+    if (!this.campaign) {
+      this.campaign = Campaign.create(this.get("vendor"), this.get("title"), this);
+      this.campaign.notiOn = false;
+    }
+    this.campaign.set("active",val);
+  });
+
+  /******************** CAMPAIGN ****************************************/
   var Campaign = Parse.Object.extend("Campaign", {
   },{ create: function (vendor, comment, card) {
     var campaign = new Campaign();
@@ -34,20 +50,25 @@ function CardsController($scope, $location, $rootScope) {
   Campaign.prototype.__defineSetter__("startDate", function(val) {return this.set("startDate",val)});
   Campaign.prototype.__defineGetter__("endDate", function() {return this.get("endDate");});
   Campaign.prototype.__defineSetter__("endDate", function(val) {return this.set("endDate",val)});
-  Campaign.prototype.__defineGetter__("active", function() {return this.get("active");});
-  Campaign.prototype.__defineSetter__("active", function(val) {return this.set("active",val)});
+  Campaign.prototype.__defineGetter__("active", function() { return this.get("active"); });
+  Campaign.prototype.__defineSetter__("active", function(val) { return this.set("active",val) });
   Campaign.prototype.__defineGetter__("notiText", function() {return this.get("notiText");});
   Campaign.prototype.__defineSetter__("notiText", function(val) {return this.set("notiText",val)});
   Campaign.prototype.__defineGetter__("notiStatus", function() {return this.get("notiStatus");});
   Campaign.prototype.__defineSetter__("notiStatus", function(val) {return this.set("notiStatus",val)});
   Campaign.prototype.__defineGetter__("notiOn", function() {
+    //console.log(this.get("comment")+" campaign Get notiOn:"+ this.get("notiStatus"));
     if (this.get("notiStatus").toLowerCase()=="wait") return true;
     if (this.get("notiStatus").substring(0,4).toLowerCase()=="sent") return true;
     return false;
   });
   Campaign.prototype.__defineSetter__("notiOn", function(val) {
-    if (val=true) return this.set("notiStatus","wait");
-    return this.set("notiStatus","off");
+    //console.log(this.get("comment")+" campaign Set notiOn: "+val );
+    if (val==true) 
+      { this.set("notiStatus","wait"); }
+    else
+      { this.set("notiStatus","off"); }
+    //return false;
   });
   
 
@@ -77,6 +98,10 @@ function CardsController($scope, $location, $rootScope) {
     });
   };
 
+  $scope.saveCard = function(card) {
+    console.log(card);
+  };
+
   function linkCardsToCampaigns(cards, campaigns) {
     for (var n=0; n<campaigns.length; n++) {
       for (var m=0; m<cards.length; m++) {
@@ -86,6 +111,7 @@ function CardsController($scope, $location, $rootScope) {
       }
     }
   }
+  /* not bused */
   function parseToCardObject (cardP, cards) {
     for (var m=0; m<cardP.length; m++) {
       var card = {
@@ -113,7 +139,7 @@ function CardsController($scope, $location, $rootScope) {
     }
     return cards;
   }
-
+  /* not used */
   function setParseGettersSetters(cards, campaigns) {
     for (var n=0; n<campaigns.length; n++) {
       Object.setPrototypeOf(campaigns[n], Campaign);
