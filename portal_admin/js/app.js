3694b34b1ee1f4e080adfcb52c132bd87d09e4fc
Bal <bal200@gmail.com>
2018-07-03 22:13:41 +0100

    switched to live db keys, and turned off Google Analytics for this admin portal.

--- portal_admin/js/app.js
+++ portal_admin/js/app.js
@@ -6,11 +6,11 @@ angular.module('lunchalert-portal', [
 
 .value('ParseConfiguration', {
     /* DEV */
-    applicationId: "uKWkJW0IFFhthG7e3A1NPqh2JhazzKEfZD7d1RXr",
-    javascriptKey: "gXgY9Kf8wafGGU3WghwqrEWMfbrl7oi7e27om7J6",
+    //applicationId: "uKWkJW0IFFhthG7e3A1NPqh2JhazzKEfZD7d1RXr",
+    //javascriptKey: "gXgY9Kf8wafGGU3WghwqrEWMfbrl7oi7e27om7J6",
     /* LIVE */
-    //applicationId: "MSfWHKif25kvcuMPxAhGBjfd7Aie1xyDe7WN6Myt",
-    //javascriptKey: "j1RTaGUP0kdj5c8iidSfrXeB7omaODiEijrDdzuC",
+    applicationId: "MSfWHKif25kvcuMPxAhGBjfd7Aie1xyDe7WN6Myt",
+    javascriptKey: "j1RTaGUP0kdj5c8iidSfrXeB7omaODiEijrDdzuC",
 		serverURL:     "https://parseapi.back4app.com/"  /* parse server to connect to */
 })
 
