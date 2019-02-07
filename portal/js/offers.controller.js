f902f00bc75403b1be281a00e94c750f09e1d6ea
Mike Carter <mike@buddasworld.co.uk>
2019-02-07 23:21:12 +0000

    Listing of existing offer cards for logged in user

--- portal/js/offers.controller.js
+++ portal/js/offers.controller.js
@@ -6,5 +6,49 @@ angular.module('lunchalert-portal')
       $location.path('/portal/offer/edit');
     }
 
+    $scope.cards = [];
+    $scope.loadCards = function () {
+      //$scope.vendors.loading=true;
+      Parse.Cloud.run("getCardsAndCampaigns", {
+          userId: ($scope.swmId ? $scope.swmId : Parse.User.current().id)
+      },{
+        success: function(res) {
+          $scope.$apply(function(){
+            linkCardsToCampaigns(res.cards, res.campaigns)
+            $scope.cards = res.cards;
+            //$scope.vendors.loading=false;
+            initTemplateVariables($scope.cards);
+          });
+        },
+        error: function(err) { console.log("Error retreiving cards and campaigns ("+err.code+") "+err.message); $scope.vendors.loading=false; }
+      });
+    };
+
+    function linkCardsToCampaigns(cards, campaigns) {
+      for (var n=0; n<campaigns.length; n++) {
+        for (var m=0; m<cards.length; m++) {
+          if (campaigns[n].get('card').id == cards[m].id) {
+            cards[m].campaign = campaigns[n];
+    } } } }
+
+    function initTemplateVariables(cards) { for (var n=0; n<cards.length; n++) { initTemplateVariable( cards[n] ) } }
+    function initTemplateVariable(card) {
+      if (card.campaign && card.campaign.get('templateVariables')) {
+        card.templateVariables = copyTemplateVars( card.campaign.get('templateVariables') );
+      }
+    }
+
+    function copyTemplateVars( from ) {
+      var to = [];
+      for (var m=0; m<from.length; m++) {  
+        to[m] = { name: from[m].name,
+                  type: from[m].type,
+                  value: (from[m].value ? from[m].value : "") };
+      }
+  
+      return to;
+    }
+
+    $scope.loadCards();
   }
 ]);
