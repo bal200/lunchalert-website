4226c7710015851d7a8fc49baf1502fe537a25ec
Mike Carter <mike@buddasworld.co.uk>
2019-02-07 22:59:40 +0000

    Added home page menu button routes

--- portal/js/controllers.js
+++ portal/js/controllers.js
@@ -3,6 +3,12 @@ angular.module('pageCtrl',['jkuri.datepicker'])
 function($scope, $location, $rootScope) {
 
   /* Put a fancy menu page here */
+  $scope.gotoMapPage = function() {
+    $location.path('/portal/map');
+  }
+  $scope.gotoOffersPage = function() {
+    $location.path('/portal/offers');
+  }
 
   if ($rootScope.isLoggedIn==false) { /* redirect if not logged in */
     $location.path('/login');
