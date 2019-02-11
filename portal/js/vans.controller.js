92d5c23e27bf2717dbf61412bb4632c1887e7741
Mike Carter <mike@buddasworld.co.uk>
2019-02-11 21:31:18 +0000

    Improved vans page message

---
+++ portal/js/vans.controller.js
@@ -0,0 +1,6 @@
+angular.module('lunchalert-portal')
+
+.controller('vansCtrl', ['$scope', '$rootScope', '$location',
+function( $scope, $rootScope, $location ) {
+  /* TODO insert clever stuff here */
+}]);
