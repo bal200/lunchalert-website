25686f8641e8db188b0f9a561731dad8f972c198
Bal <bal200@gmail.com>
2019-02-14 00:27:05 +0000

    work in progress

--- portal/js/offerwizard.controller.js
+++ portal/js/offerwizard.controller.js
@@ -3,7 +3,7 @@ angular.module('lunchalert-portal')
 .controller('offerWizardCtrl', ['$scope',  '$location', '$rootScope',
   function($scope,  $location, $rootScope) {
 
-    $scope.currentCard = null;
+    $scope.card = null;
     $scope.page = 1;
 
     $scope.pageNext = function() {
@@ -13,18 +13,49 @@ angular.module('lunchalert-portal')
       if ($scope.page > 1) $scope.page--;
     }
 
+    var loadTemplate = function() {
+      var query = new Parse.Query(CardTemplate);
+      query.equalTo("vendor", this.vendor );  /* templates for this vendor  */
 
-
-    $scope.currentCard = $rootScope.currentCard; //$stateParams.card;
-
-    if ($scope.currentCard == null) {
-      $scope.currentCard = Card.create(getCurrentVendor(), "", 1);
+      query.find().then(function(templates) {
+        $scope.$apply(function() {
+          console.log("got "+templates.length+" templates");
+          var template = templates[0];
+          $scope.card.campaign.template = template;
+        });
+      });
     }
+
+    $scope.card = $rootScope.card; //$stateParams.card;
+
+    if ($scope.card == null) {
+      $scope.card = Card.create(getCurrentVendor(), "", 1);
+      $scope.card.campaign = Campaign.create($scope.card.get("vendor"), "", $scope.card);
+      $scope.card.schedulerOn = false; /* this also causes the campaign to get set to defaults */
+      //card.campaign.template = template;
+      loadTemplate();
+    }
+
+    $scope.tVars = { 
+      get title() { 
+        return $scope.card.title; 
+      },
+      set title(val) { 
+        $scope.card.title=val; 
+      },
+      get message() { 
+        //return $scope.card.campaign.templateVariables; 
+      },
+      set message(val) { 
+        //$scope.card.title=val; 
+      }
+    };
+    
+
 
     function getCurrentVendor() {
       return $scope.swmId ? newParseUser($scope.swmId) : Parse.User.current();
     }
-
 
   }
 ]);
