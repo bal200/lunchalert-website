e6476482fbf973339d433c1ac0d1b116d7a69804
Mike Carter <mike@buddasworld.co.uk>
2019-02-16 17:44:33 +0000

    Changed van list code - it doesn't work yet, needs some TLC

--- portal/js/vans.controller.js
+++ portal/js/vans.controller.js
@@ -5,6 +5,7 @@ function( $scope, $rootScope, $location, $filter ) {
 
   var usrObj = Parse.User.current();
   var swm = usrObj.id;
+  var vendorObj = $scope.swmId ? newParseUser($scope.swmId) : Parse.User.current();
 
   /* Work out the difference in days between two dates */
   $scope.differenceInDays = function(firstdate, seconddate) {
@@ -20,22 +21,43 @@ function( $scope, $rootScope, $location, $filter ) {
 
     return Math.floor(days);
   };
-  
+
   $scope.vanList=[];
 
-  Parse.Cloud.run("getVanList", {
-      userid: swm
-  },{
-    success:function(res) {
-      $scope.$apply(function () {
-        var flag = '';
-        $scope.activedrivers = 0;
-        for (var i = 0; i < res.length; i++) {
-          console.log(res[i]);
+  // obtain a list of all registered vans for the vendor
+  var Van = Parse.Object.extend("Van");
+  var query = new Parse.Query(Van);
+  query.equalTo("vendor", vendorObj);
+  query.find({
+    success: function(vendorVans) 
+    {
+/*
+for each van in result
+create a query to get the vans updatedat info from the Arrival table
+add the array of van querys as a query._orQuery( vendorQuery ) and execute it
+put results in to van object for rendering as a list
+*/
+      var Arrival = Parse.Object.extend("Arrival");
+      var arrivalQueries = [];
 
-          /* Work out status flag based on date */
+      // Obtain a list of last stop for each van in a vendors fleet
+      for(var i = 0; i < vendorVans.length; i++) {
+        arrivalQueries[i] = new Parse.Query(Arrival);
+        arrivalQueries[i].equalTo("vanId", vendorVans[i].get('vanId'));
+      }
+
+      // One query to rule all the vans
+      var query = new Parse.Query(Arrival);
+      query._orQuery( arrivalQueries );
+      query.descending('updatedAt');
+      query.find({ success: function(vanArrivals) {
+
+        console.log(vanArrivals); // TODO currently returns 100 results - needs to be just one per van (7)
+
+        for(var o = 0; o < vanArrivals.length; o++) {
+          // Work out status flag based on date
           f = $scope.differenceInDays(
-            $filter('date')(res[i].get("updatedAt"), "dd/MM/yyyy"),
+            $filter('date')(vanArrivals[o].get("updatedAt"), "dd/MM/yyyy"),
             $filter('date')(new Date, "dd/MM/yyyy")
           );
           if(f < 2) {
@@ -47,20 +69,69 @@ function( $scope, $rootScope, $location, $filter ) {
             flag = 'negative';
           }
 
-          van = {
-              id: i,
-              objId: res[i].id,
-              vanId: res[i].get("vanId"),
-              vanIdCrop: res[i].get("vanId").substr(0,5),
-              name: res[i].get("name"),
-              vendor: res[i].get("vendor"),
-              active: res[i].get("updatedAt"),
-              flag: flag,
+          var van = {
+            id: o,
+            objId: vanArrivals[o].id,
+            vanId: vanArrivals[o].get("vanId"),
+            vanIdCrop: vanArrivals[o].get("vanId").substr(0,5),
+            name: '', //vendorVans[o].get("name"),
+            vendor: vanArrivals[o].get("vendor"),
+            active: vanArrivals[o].get("updatedAt"),
+            flag: flag,
           };
           $scope.vanList.push( van );
         }
-      });
+
+        // A list of all the vans 
+        console.log('a list of vans and their last arrival times...');
+        console.log($scope.vanList);
+
+      }, error: function(err) {response.error("Van last arrival List error: "+err.code+" "+err.message);}});
     },
-    error: function(err) { alert("get Vans error: "+err.code+" "+err.message); }
+    error: function(err) {response.error("find van List error: "+err.code+" "+err.message);}
   });
+
+
+
+
+  // Parse.Cloud.run("getVanList", {
+  //     userid: swm
+  // },{
+  //   success:function(res) {
+  //     $scope.$apply(function () {
+  //       var flag = '';
+  //       $scope.activedrivers = 0;
+  //       for (var i = 0; i < res.length; i++) {
+  //         console.log(res[i]);
+
+  //         /* Work out status flag based on date */
+  //         f = $scope.differenceInDays(
+  //           $filter('date')(res[i].get("updatedAt"), "dd/MM/yyyy"),
+  //           $filter('date')(new Date, "dd/MM/yyyy")
+  //         );
+  //         if(f < 2) {
+  //           flag = 'positive';
+  //           $scope.activedrivers++;
+  //         }else if(f <= 3) {
+  //           flag = 'warning';
+  //         }else {
+  //           flag = 'negative';
+  //         }
+
+  //         van = {
+  //             id: i,
+  //             objId: res[i].id,
+  //             vanId: res[i].get("vanId"),
+  //             vanIdCrop: res[i].get("vanId").substr(0,5),
+  //             name: res[i].get("name"),
+  //             vendor: res[i].get("vendor"),
+  //             active: res[i].get("updatedAt"),
+  //             flag: flag,
+  //         };
+  //         $scope.vanList.push( van );
+  //       }
+  //     });
+  //   },
+  //   error: function(err) { alert("get Vans error: "+err.code+" "+err.message); }
+  // });
 }]);
