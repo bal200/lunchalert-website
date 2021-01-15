a68ed13b859ed478112043e6e4bdab9fe809e591
Bal <bal200@gmail.com>
2021-01-15 01:21:28 +0000

    Cards page wip

--- vue-portal/src/views/CardsPage.vue
+++ vue-portal/src/views/CardsPage.vue
@@ -3,12 +3,64 @@
 		<h1>Cards</h1>
 		<div v-if="loading">loading...</div>
 		<div v-else>
-			<p>Active Cards</p>
-			<div v-for="card in activeCards" :key="card.id">
+
+			<p>Active and Future Cards</p>
+			<div class="list-group">
+				<a v-for="card in activeCards" :key="card.id" click.prevent.stop href=""
+						class="list-group-item list-group-item-action">
+					<div class="media">
+						<CardPreview :html="card.html" :card="card" :scrollbar="false" myWidth="150" myHeight="100" class="mr-3" />
+						<div class="media-body">
+							<h5 class="mt-0">
+								{{card.title}}
+								<small><span class="badge badge-success">{{statusBadge(card)}}</span></small>
+								<small><i> {{card.order}}</i></small>
+							</h5>
+							<!-- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> -->
+							<div class="mt-2">
+								<div class="btn-group btn-group-toggle mr-2">
+									<label class="btn btn-secondary btn-sm active">
+										<input type="radio" name="options" id="option1" autocomplete="off" checked> On
+									</label>
+									<label class="btn btn-secondary btn-sm">
+										<input type="radio" name="options" id="option2" autocomplete="off"> Off
+									</label>
+									<label class="btn btn-secondary btn-sm">
+										<input type="radio" name="options" id="option3" autocomplete="off"> Scheduled
+									</label>
+								</div>
+								
+								<button type="button" class="btn btn-primary btn-sm mr-2">Edit</button>
+								<button type="button" class="btn btn-secondary btn-sm">Run again</button>
+							</div>
+							<form class="mt-2">
+								<div class="row">
+									<div class="col">
+										<input type="text" class="form-control" placeholder="Start Date">
+									</div>
+									<div class="col">
+										<input type="text" class="form-control" placeholder="End Date">
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+
+				<!-- <div class="d-flex w-100 justify-content-between">
+					<h5 class="mb-1">{{card.comment}}</h5>
+					<small>{{card.active}}</small>
+				</div>
+				<p class="mb-1">Donec</small> -->
+				
+				</a>
+			</div>
+
+
+			<!-- <div v-for="card in activeCards" :key="card.id">
 				<p>{{card.id}} - {{card.comment}} - {{card.active}}</p>
 				<CardPreview :html="card.html" :card="card" :scrollbar="false"
 						myWidth="150" myHeight="100" />
-			</div>
+			</div> -->
 			<p>Past Cards</p>
 			<div v-for="card in pastCards" :key="card.id">
 				<p>{{card.id}} - {{card.comment}} - {{card.active}}</p>
@@ -48,6 +100,12 @@ export default class CardsPage extends Vue {
 	}
 	get pastCards() {
 		return this.cards.filter( card => !card.active )
+	}
+	statusBadge(card:Card) {
+		if (card.active) return "active";
+		if (card.schedulerActive && !card.active) return "scheduled";
+		if (!card.active) return "off";
+		else return "";
 	}
 	
 	loadCards() {
